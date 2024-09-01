9c1da1614b89957184ef5e6f2f6cf3d20146689a
havel <thakurankit13197@gmail.com>
2024-09-01 22:01:18 +0530

    first commit

---
+++ components/user-avatar.tsx
@@ -0,0 +1,27 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+
+interface UserAvatarProps {
+    src?: string | null;
+    className?: string;
+};
+
+export const UserAvatar = ({
+    src,
+    className
+}: UserAvatarProps ) => {
+
+    if (!src) {
+        return null;
+    }
+
+    return (  
+        <Avatar  className={cn(
+            "h-7 w-7 md:h-10 md:w-10",
+            className
+        )}>
+            <AvatarImage src={src} />
+        </Avatar>
+    );
+}
+ 
